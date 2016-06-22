a942073ff0b1cd6443b53c1f6e47e828081b6bec
johnmanzano <johns_166@hotmail.com>
2016-06-22 08:12:40 +0100

    Creating Project
    Tutorial project with basic entity models for the book store. Need to
    create controllers.

---
+++ BookService/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        public int Id { set; get; }
+
+        [Required]
+        public string Title { set; get; }
+
+        public int Year { set; get; }
+
+        
+        public decimal Price { set; get; }
+
+
+        public string Genre { set; get; }
+
+        // Foreign Key
+        public int  AuthorId { set; get; }
+
+        // Navigation property
+        public Author Author { set; get; }
+
+    }
+}
