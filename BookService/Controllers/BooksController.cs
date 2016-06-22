ae88fb388fbdaea74351fc8bdcea57c23b609a5b
johnmanzano <johns_166@hotmail.com>
2016-06-22 19:38:47 +0100

    Code First Approach
    Creating database and table from models

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -20,7 +20,9 @@ namespace BookService.Controllers
         // GET: api/Books
         public IQueryable<Book> GetBooks()
         {
-            return db.Books;
+            return db.Books
+                .Include(b => b.Author);
+            
         }
 
         // GET: api/Books/5
