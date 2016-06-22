b07927e38b9b01bf4d02f2f643b2df21aee62616
johnmanzano <johns_166@hotmail.com>
2016-06-22 18:33:20 +0100

    Adding  Web API controllers
    Adding Book and Author Controllers

--- BookService/Models/Author.cs
+++ BookService/Models/Author.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookService.Models
 {
-    class Author
+    public class Author
     {
+        public int Id { set; get; }
+
+        [Required]
+        public string Name { set; get; }
     }
 }
